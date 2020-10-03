e3ff0ecfd277d88a405d49811d3cd6d6d2cc920a
Marcin Korbut <coreboot@tlen.pl>
2020-10-03 20:51:08 +0200

    sqlite first tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'sqlite.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -60,6 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      sqliteTest();
     });
   }
 
