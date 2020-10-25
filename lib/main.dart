178a3a688f06494e61791960bdfe986fba69ad2a
CoreBoot <coreboot@tlen.pl>
2020-10-26 00:01:10 +0100

    added Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -89,6 +89,9 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      drawer: Drawer(
+        child: Text ('Siema')
+      ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
